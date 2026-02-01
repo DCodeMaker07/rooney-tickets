0e1a79fe4806a752a2b5316296f1b6aadc7a558f
DelioCoder <davideveriwhere@gmail.com>
2026-02-01 17:52:44 -0500

    Chech-Access use case added

--- src/contexts/queue/infrastructure/queue.module.ts
+++ src/contexts/queue/infrastructure/queue.module.ts
@@ -5,12 +5,14 @@ import { QueueController } from './controllers/queue.controller';
 import { QueueRepository } from '../domain/queue.repository';
 import { JoinQueueUseCase } from '../application/join-queue.use-case/join-queue.use-case';
 import { GrantAccessUseCase } from '../application/grant-access-use-case/grant-access-use-case';
+import { CheckAccessUseCase } from '../application/check-access-use-case/check-access-use-case';
 
 @Module({
     controllers: [QueueController],
     providers: [
         RedisQueueRepositoryAdapter,
         JoinQueueUseCase,
+        CheckAccessUseCase,
         GrantAccessUseCase,
         {
             provide: QueueRepository,
