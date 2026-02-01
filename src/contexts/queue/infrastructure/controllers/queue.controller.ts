64e98da52b22db342a4dd84db4abad99786aaff2
DelioCoder <davideveriwhere@gmail.com>
2026-02-01 17:52:35 -0500

    Check-Access controller added

--- src/contexts/queue/infrastructure/controllers/queue.controller.ts
+++ src/contexts/queue/infrastructure/controllers/queue.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Param, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Req } from "@nestjs/common";
 import { JoinQueueUseCase } from '../../application/join-queue.use-case/join-queue.use-case';
 import { GrantAccessUseCase } from '../../application/grant-access-use-case/grant-access-use-case';
+import { CheckAccessUseCase } from '../../application/check-access-use-case/check-access-use-case';
 
 @Controller('concerts')
 export class QueueController {
@@ -8,17 +9,25 @@ export class QueueController {
     constructor(
         private readonly joinQueueUseCase: JoinQueueUseCase,
         private readonly grantAccessUseCase: GrantAccessUseCase,
+        private readonly checkAccessUseCase: CheckAccessUseCase,
     ) { }
 
+    @Get(':concertId/access')
+    checkAccess(@Param('concertId') concertId: string, @Req() req) {
+        const userId = req.user?.id ?? req.headers['x-user-id'];
+
+        return this.checkAccessUseCase.execute(concertId, userId);
+    }
+
     @Post(':concertId/queue')
-    async join(@Param('concertId') concertId: string, @Req() req) {
+    join(@Param('concertId') concertId: string, @Req() req) {
         const userId = req.user?.id ?? req.headers['x-user-id'];
 
         return this.joinQueueUseCase.execute(concertId, userId);
     }
 
     @Post(':concertId/grant')
-    async grantAcces(@Param('concertId') concertId: string, @Body('limit') limit: number) {
+    grantAcces(@Param('concertId') concertId: string, @Body('limit') limit: number) {
         return this.grantAccessUseCase.execute(concertId, limit);
     }
 
