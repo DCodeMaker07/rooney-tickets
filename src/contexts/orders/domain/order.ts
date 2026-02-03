e1b7609103b735ae7a0a30ee318a4103e54b1709
DelioCoder <davideveriwhere@gmail.com>
2026-02-02 22:13:55 -0500

    static method to parse entity

--- src/contexts/orders/domain/order.ts
+++ src/contexts/orders/domain/order.ts
@@ -1,26 +1,40 @@
 export type OrderStatus = 'PENDING' | 'PAID' | 'CANCELLED';
 
 export class Order {
-    
+
     constructor(
         public readonly userId: string,
         public readonly concertId: string,
         public readonly amount: number,
         public status: OrderStatus,
         public readonly expiresAt: Date,
-        public readonly paidAt?: Date,
+        public readonly id?: string,
+        public paidAt?: Date,
     ) { }
+
+    public static toOrder(input: {
+        id: string;
+        status: OrderStatus;
+        total: number;
+        createdAt: Date;
+        paidAt: Date | null;
+        userId: string;
+        concertId: string;
+    }) {
+        return new Order(input.userId, input.concertId, input.total, input.status, new Date(), input.id, input.paidAt!);
+    }
 
     isExpired(now = new Date()): boolean {
         return now > this.expiresAt;
     }
 
     markAsPaid() {
-        if(this.status !== 'PENDING'){
+        if (this.status !== 'PENDING') {
             throw new Error('Order cannot be paid');
         }
 
         this.status = 'PAID';
+        this.paidAt = new Date();
     }
 
 }
