a63ef35ebf30ec6b4b2a4e5ca300d2b3f3031dc4
DelioCoder <davideveriwhere@gmail.com>
2026-02-02 20:38:25 -0500

    EventEmitter added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { PaymentModule } from './contexts/payments/infrastructure/payment.module';
 import { QueueModule } from './contexts/queue/infrastructure/queue.module';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot({
+      wildcard: true, // 'order.*'
+      delimiter: '.', // default
+      newListener: false,
+      removeListener: false,
+      maxListeners: 20, // listeners
+      verboseMemoryLeak: true,  // for debugging
+      ignoreErrors: false,
+    }),
     PaymentModule,
     QueueModule,
   ],
