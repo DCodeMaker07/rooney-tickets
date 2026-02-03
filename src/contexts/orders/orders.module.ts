9418502bb0d0b1058268e2d81064d2d4a418ad09
DelioCoder <davideveriwhere@gmail.com>
2026-02-02 22:12:43 -0500

    Dependencies & useCases added

--- src/contexts/orders/orders.module.ts
+++ src/contexts/orders/orders.module.ts
@@ -1,9 +1,22 @@
 
 import { Module } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+import { CreateOrderUseCase } from './application/create-order-use-case/create-order-use-case';
+import { OrderRepository } from './domain/order.repository';
+import { OrderRepositoryAdapter } from './infrastructure/database/postgresql/order-repository-adapter';
+import { OrdersController } from './infrastructure/controller/orders.controller';
 
 @Module({
-    controllers: [],
+    controllers: [OrdersController],
     imports: [],
-    providers: []
+    providers: [
+        PrismaService,
+        CreateOrderUseCase,
+        OrderRepositoryAdapter,
+        {
+            provide: OrderRepository,
+            useExisting: OrderRepositoryAdapter,
+        }
+    ]
 })
 export class OrdersModule { }
