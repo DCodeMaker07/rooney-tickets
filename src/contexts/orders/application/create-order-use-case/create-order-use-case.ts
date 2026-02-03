d99b822d7316bc5389fdd2a150a4e087d5666a95
DelioCoder <davideveriwhere@gmail.com>
2026-02-02 22:13:22 -0500

    Nest EventEmitter added

--- src/contexts/orders/application/create-order-use-case/create-order-use-case.ts
+++ src/contexts/orders/application/create-order-use-case/create-order-use-case.ts
@@ -1,22 +1,29 @@
 import { Injectable } from "@/common/Injectable";
+import { EventEmitter2 } from "@nestjs/event-emitter";
 import { OrderRepository } from "../../domain/order.repository";
 import { CreateOrderInput } from "./create-order-input";
 import { Order } from "../../domain/order";
+import { OrderCreatedEvent } from "../../domain/order.event";
 
 @Injectable()
 export class CreateOrderUseCase {
 
     constructor(
         private readonly orderRepository: OrderRepository,
+        private eventEmitter: EventEmitter2,
     ) { }
 
     async execute(input: CreateOrderInput) {
 
         const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
 
-        const order = new Order(input.userId, input.concertId, input.total, 'PENDING', expiresAt);
+        const order = new Order(input.userId, input.concertId, input.amount, 'PENDING', expiresAt);
 
         const createdOrder = await this.orderRepository.create(order);
+
+        this.eventEmitter.emit('order.created', new OrderCreatedEvent(createdOrder.id!, createdOrder.amount, 'STRIPE', 'test', 'USD'));
+
+        return createdOrder;
 
     }
 
