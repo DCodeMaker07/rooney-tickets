93db31e0e29cd8fa6ab7e8797e541fe71416932d
DelioCoder <davideveriwhere@gmail.com>
2026-02-07 15:42:24 -0500

    Unused methods removed

--- src/contexts/payments/application/create-payment-input.ts
+++
@@ -1,9 +0,0 @@
-import { PaymentProvider } from "../domain/payment";
-
-export interface CreatePaymentInput {
-    orderId: string;
-    amount: number;
-    provider: PaymentProvider;
-    externalId: string;
-    currency?: string;
-}
