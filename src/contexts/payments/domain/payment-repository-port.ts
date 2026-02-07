0db6e4ea6bfe640a8ae26b7788e2ec5bdf5e17b8
DelioCoder <davideveriwhere@gmail.com>
2026-02-07 15:43:44 -0500

    Partial type added to parameter

--- src/contexts/payments/domain/payment-repository-port.ts
+++ src/contexts/payments/domain/payment-repository-port.ts
@@ -2,7 +2,7 @@ import { Payment } from "./payment";
 
 export abstract class PaymentRepository {
   abstract create(payment: Payment): Promise<Payment>;
-  abstract update(payment: Payment): Promise<void>;
+  abstract update(payment: Partial<Payment>): Promise<void>;
   abstract findByOrderId(orderId: string): Promise<Payment | null>;
   abstract findByExternalId(externalId: string): Promise<Payment | null>;
 }
