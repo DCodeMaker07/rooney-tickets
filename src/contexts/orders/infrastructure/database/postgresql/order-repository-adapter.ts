33ab4bf06050d1a3d670b482f6c87941446297e0
DelioCoder <davideveriwhere@gmail.com>
2026-02-08 07:06:50 -0500

    seats verification added

--- src/contexts/orders/infrastructure/database/postgresql/order-repository-adapter.ts
+++ src/contexts/orders/infrastructure/database/postgresql/order-repository-adapter.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@/common/Injectable";
 import { Order } from "@/contexts/orders/domain/order";
 import { OrderRepository } from "@/contexts/orders/domain/order.repository";
 import { PrismaService } from "@/prisma/prisma.service";
+import { NotFoundException } from "@nestjs/common";
 
 @Injectable()
 export class OrderRepositoryAdapter implements OrderRepository {
@@ -11,12 +12,25 @@ export class OrderRepositoryAdapter implements OrderRepository {
     ) { }
 
     async create(order: Order): Promise<Order> {
+
+        const seatsId = order.items.map((item) => {
+            return item.seatId
+        });
+
+        const seats = await this.prisma.seat.findMany({
+            where: {
+                id: { in: seatsId },
+            }
+        });
+        
+        if(seats.length === 0) throw new NotFoundException(`Invalid seats`);
+        
         const created = await this.prisma.order.create({
             data: {
                 userId: order.userId,
                 concertId: order.concertId,
                 status: order.status,
-                total: order.amount,
+                total: order.total,
                 orderItem: {
                     createMany: {
                         data: order.items.map((item) => ({
@@ -46,6 +60,7 @@ export class OrderRepositoryAdapter implements OrderRepository {
                 seatId: u.seatId,
             }))
         });
+
     }
     async update(order: Order): Promise<void> {
         throw new Error("Method not implemented.");
