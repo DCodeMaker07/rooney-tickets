c6ee65dcd2fa79145929ed1841270f2e3caa271d
DelioCoder <davideveriwhere@gmail.com>
2026-02-08 07:07:01 -0500

    clientSecret added

--- src/contexts/payments/domain/payment-gateway-port.ts
+++ src/contexts/payments/domain/payment-gateway-port.ts
@@ -2,8 +2,10 @@ export abstract class PaymentGateway {
     abstract createPayment(input: {
         orderId: string;
         amount: number;
+        paymentId: string;
     }): Promise<{
         externalId: string;
-        status: string
+        status: string;
+        clientSecret: string;
     }>;
 }
