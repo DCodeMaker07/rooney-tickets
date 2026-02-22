2d3b575ee05c5559afa91f745fa086512e9a7707
DelioCoder <davideveriwhere@gmail.com>
2026-02-22 17:09:44 -0500

    orderId & paymentId added in input

--- src/contexts/payments/application/update-payment-use-case/update-payment-input.ts
+++ src/contexts/payments/application/update-payment-use-case/update-payment-input.ts
@@ -2,6 +2,8 @@ import { PaymentProvider, PaymentStatus } from "../../domain";
 
 export interface UpdatePaymentInput {
 
+    orderId: string;
+    paymentId: string;
     provider: PaymentProvider;
     externalId: string;
     status: PaymentStatus;
